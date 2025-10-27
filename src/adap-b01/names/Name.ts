8a222c2837966cdb29d330c5a8ae74c427549587
JohannesA15 <johannes.ackermann@fau.de>
2025-10-27 15:14:14 +0100

    Revert "Homework b01 Names"
    This reverts commit d12a962f492ab11802bafd2a1f6da02c106dbe71.

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -1,118 +1,72 @@
-export const DEFAULT_DELIMITER: string = ".";
-export const ESCAPE_CHARACTER = "\\";
+export const DEFAULT_DELIMITER: string = '.';
+export const ESCAPE_CHARACTER = '\\';
 
 /**
  * A name is a sequence of string components separated by a delimiter character.
  * Special characters within the string may need masking, if they are to appear verbatim.
  * There are only two special characters, the delimiter character and the escape character.
  * The escape character can't be set, the delimiter character can.
- *
+ * 
  * Homogenous name examples
- *
+ * 
  * "oss.cs.fau.de" is a name with four name components and the delimiter character '.'.
  * "///" is a name with four empty components and the delimiter character '/'.
  * "Oh\.\.\." is a name with one component, if the delimiter character is '.'.
  */
 export class Name {
-  private delimiter: string = DEFAULT_DELIMITER;
-  private components: string[] = [];
 
-  /** Expects that all Name components are properly masked */
-  // @methodtype initialization-method
-  constructor(other: string[], delimiter?: string) {
-    // Shallow copy
-    this.components = [...other];
-    if (delimiter) {
-      this.delimiter = delimiter;
-    }
-  }
+    private delimiter: string = DEFAULT_DELIMITER;
+    private components: string[] = [];
 
-  /**
-   * Returns a human-readable representation of the Name instance using user-set control characters
-   * Control characters are not escaped (creating a human-readable string)
-   * Users can vary the delimiter character to be used
-   */
-  // @methodtype conversion-method
-  public asString(delimiter: string = this.delimiter): string {
-    return this.components.join(delimiter);
-  }
-
-  /**
-   * Returns a machine-readable representation of Name instance using default control characters
-   * Machine-readable means that from a data string, a Name can be parsed back in
-   * The control characters in the data string are the default characters
-   */
-  // @methodtype conversion-method
-  public asDataString(): string {
-    let result = "";
-
-    for (let i = 0; i < this.getNoComponents(); i++) {
-      let component = this.getComponent(i);
-      let escapedComponent = "";
-
-      for (let char of component) {
-        if (char === ESCAPE_CHARACTER || char === DEFAULT_DELIMITER) {
-          escapedComponent += ESCAPE_CHARACTER;
-        }
-
-        escapedComponent += char;
-      }
-
-      result += escapedComponent;
-
-      // Add delimiter if not the last component
-      if (i < this.getNoComponents() - 1) {
-        result += DEFAULT_DELIMITER;
-      }
+    /** Expects that all Name components are properly masked */
+    constructor(other: string[], delimiter?: string) {
+        throw new Error("needs implementation or deletion");
     }
 
-    return result;
-  }
-
-  // @methodtype get-method
-  public getComponent(i: number): string {
-    this.assertIsValidIndex(i);
-    return this.components[i];
-  }
-
-  /** Expects that new Name component c is properly masked */
-  // @methodtype set-method
-  public setComponent(i: number, c: string): void {
-    this.assertIsValidIndex(i);
-    this.components[i] = c;
-  }
-
-  /** Returns number of components in Name instance */
-  // @methodtype get-method
-  public getNoComponents(): number {
-    return this.components.length;
-  }
-
-  /** Expects that new Name component c is properly masked */
-  // @methodtype command-method
-  public insert(i: number, c: string): void {
-    if (i < 0 || i > this.getNoComponents()) {
-      throw new RangeError("Invalid index value");
+    /**
+     * Returns a human-readable representation of the Name instance using user-set control characters
+     * Control characters are not escaped (creating a human-readable string)
+     * Users can vary the delimiter character to be used
+     */
+    public asString(delimiter: string = this.delimiter): string {
+        throw new Error("needs implementation or deletion");
     }
-    this.components.splice(i, 0, c);
-  }
 
-  /** Expects that new Name component c is properly masked */
-  // @methodtype command-method
-  public append(c: string): void {
-    this.components.push(c);
-  }
-
-  // @methodtype command-method
-  public remove(i: number): void {
-    this.assertIsValidIndex(i);
-    this.components.splice(i, 1);
-  }
-
-  // @methodtype helper-method
-  private assertIsValidIndex(i: number): void {
-    if (i < 0 || i >= this.getNoComponents()) {
-      throw new RangeError("Invalid index value");
+    /** 
+     * Returns a machine-readable representation of Name instance using default control characters
+     * Machine-readable means that from a data string, a Name can be parsed back in
+     * The control characters in the data string are the default characters
+     */
+    public asDataString(): string {
+        throw new Error("needs implementation or deletion");
     }
-  }
+
+    public getComponent(i: number): string {
+        throw new Error("needs implementation or deletion");
+    }
+
+    /** Expects that new Name component c is properly masked */
+    public setComponent(i: number, c: string): void {
+        throw new Error("needs implementation or deletion");
+    }
+
+     /** Returns number of components in Name instance */
+     public getNoComponents(): number {
+        throw new Error("needs implementation or deletion");
+    }
+
+    /** Expects that new Name component c is properly masked */
+    public insert(i: number, c: string): void {
+        throw new Error("needs implementation or deletion");
+    }
+
+    /** Expects that new Name component c is properly masked */
+    public append(c: string): void {
+        throw new Error("needs implementation or deletion");
+    }
+
+    public remove(i: number): void {
+        throw new Error("needs implementation or deletion");
+    }
+
 }
